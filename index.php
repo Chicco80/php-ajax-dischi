02dd5c25251745c142cd47c1ff1297d3ff2b61bc
Chicco80 <chicco80lisa@gmail.com>
2022-05-25 16:07:31 +0200

    card-php

--- index.php
+++ index.php
@@ -17,18 +17,17 @@ include __DIR__.'./database.php';
     <script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
     <title>php-ajax-dischi</title>
 </head>
-<body>
-    <h1>Dischi</h1>
-    <div class="container">
+<body class="color">
+    
+    <div class="container pt-5 mt-5">
         <div class="row">
             <?php foreach($dischi as $disco){ ?>
             <div class="col-3">
-            <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
+            <div class="card bg debug my-3" style="width: 18rem;">
+                <img src="<?php echo $disco['poster']?>" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php echo $disco['author']?></h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <h5 class="card-title"><?php echo $disco['title']?></h5>
+                        <p class="card-text"><?php echo $disco['author']?></p>
                     </div>
                 </div>
             </div>
