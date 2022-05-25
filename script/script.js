bfad612f6bbb869f9d4e01327f7ee696d95eb075
Chicco80 <chicco80lisa@gmail.com>
2022-05-25 17:01:18 +0200

    card-html

--- script/script.js
+++ script/script.js
@@ -1,13 +1,16 @@
-// const app = new Vue({
-//     el: '#app',
-//     data:{
-//       dischi: [],
-//     },
-//     methods:{
-  
-//     },
-//     mounted(){
-  
-//     }
-//   }) 
-alert('bfhjbfhnf')
+const app = new Vue({
+    el: '#app',
+    data:{
+        dischi:[],
+    },
+        methods:{
+
+    },
+        mounted(){
+            axios.get('server.php').then((res =>{
+                this.dischi = res.data;
+                console.log(this.dischi);
+            }))
+
+    }
+}) 
